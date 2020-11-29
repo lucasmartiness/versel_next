20a998c45526b0bb64bf3f9b2aad2244294df54e
lucasmartines <lucas@plyn.com.br>
2020-11-29 20:37:30 -0300

    novo commit

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
+import Image from 'next/image'
+
+
 function HomePage() {
-   return <div>Welcome to future !</div>
+   return <div><Image src="/imgs/me.jpg" alt="me" width="64" height="64" />   </div>
 }
 
 export default HomePage
